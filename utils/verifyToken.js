de751db315fe25df0da5100ad573fabb68ec334c
brunash <bobazso@yahoo.com>
2022-08-16 08:31:56 -0400

    minor change

--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -7,7 +7,7 @@ export const verifyToken = (req,res,next)=> {
         return next(createError(401, "You are not authenticated"))
     }
     jwt.verify(token,process.env.JWT, (err, user)=> {
-        if(err) return next(createError(401, "Token not valid!"));
+        if(err) return next(createError(403, "Token not valid!"));
         req.user = user;
         next()
     })
