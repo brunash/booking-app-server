cae81f17483bb9df0b1143f93a64ab400b95b946
brunash <bobazso@yahoo.com>
2022-08-16 21:45:37 -0400

    adding authorization

--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,34 @@
 import express from "express";
 import { updateUser, deleteUser, getUser, getAllUsers } from "../controllers/user.js";
-import { verifyToken } from "../utils/verifyToken.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
 
-router.get("/checkauth", verifyToken, (req,res,next)=> {
-    res.send("Hello user, you are logged in")
-}
-)
+// router.get("/checkauth", verifyToken, (req,res,next)=> {
+//     res.send("Hello user, you are logged in")
+// }
+// );
+
+// router.get("/checkuser/:id", verifyUser, (req,res,next)=> {
+//     res.send("Hello user, you are logged in and you can delete your account.")
+// }
+// );
+
+// router.get("/checkadmin/:id", verifyAdmin, (req, res, next) => {
+//   res.send("Hello admin, you are logged in and you can delete all accounts.");
+// });
+
 //UPDATE
-router.put("/:id", updateUser);
+router.put("/:id", verifyUser, updateUser);
 
 //DELETE
-router.delete("/:id", deleteUser);
+router.delete("/:id", verifyUser, deleteUser);
 
 //GET SPECIFIC User
-router.get("/:id", getUser);
+router.get("/:id", verifyUser, getUser);
 
 //GET ALL
-router.get("/", getAllUsers);
+router.get("/", verifyAdmin, getAllUsers);
 
 export default router;
