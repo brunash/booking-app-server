3592b6f08221ae6ec0d741f89eab701f8a367017
Slobodan Zaja <bobazso@yahoo.com>
2022-08-23 22:20:14 -0400

    added room models and controllers

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,5 +1,25 @@
 import express from "express";
+import { verifyAdmin } from "../utils/verifyToken.js";
+import { createRoom, deleteRoom, updateRoom, getRoom, getAllRooms } from "../controllers/room.js";
 
 const router = express.Router();
+
+
+//CREATE
+router.post("/:hotelId", verifyAdmin, createRoom);
+
+//UPDATE
+router.put("/:id", verifyAdmin,  updateRoom);
+
+//DELETE
+router.delete("/:id", verifyAdmin, deleteRoom);
+
+//GET SPECIFIC HOTEL
+router.get("/:id", getRoom);
+
+//GET ALL 
+//
+router.get("/", getAllRooms);
+
 
 export default router;
